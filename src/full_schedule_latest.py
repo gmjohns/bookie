bc5b0ebcafac11e05d2b8f302b6001d140b7b7d7
DESKTOP-9IRCMAK\Garrett <gmjohns@clemson.edu>
2019-08-29 08:02:49 -0500

    remove old file

--- src/full_schedule_latest.py
+++
@@ -1,6 +0,0 @@
-from ohmysportsfeedspy import MySportsFeeds
-
-msf = MySportsFeeds(version="1.2")
-msf.authenticate("a91b62dd-084f-4eb8-b1ed-cdcd1c", "Thebigbob11")
-
-output = msf.msf_get_data(league='mlb',season='latest',feed='full_game_schedule',format='csv')
